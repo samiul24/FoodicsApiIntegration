235eabaca553b3ccaafb59ff27ebf5b62df7f2dc
Md Samiul Islam <36325366+samiul24@users.noreply.github.com>
2023-10-28 13:08:16 +0600

    28_10_2023

--- FoodicsApiIntegration.bat
+++ FoodicsApiIntegration.bat
@@ -1,5 +1,9 @@
 @echo off
-echo Start Sync: %DATE% %TIME%
+
+set mmddyyyy=%DATE:~4,2%%DATE:~7,2%%DATE:~10,4%
+
+echo Start Sync: %DATE% %TIME% >> Logs\log_%mmddyyyy%.txt
+
 "C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Inventory_Categories.py"
 "C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Inventory_Items.py"
 "C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Inventory_Transaction.py"
@@ -8,4 +12,9 @@ echo Start Sync: %DATE% %TIME%
 "C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Products.py"
 "C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Purchase_Orders.py"
 "C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Transfer_Orders.py"
-echo End Sync: %DATE% %TIME%
+"C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Modifiers.py"
+"C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Discounts.py"
+"C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Addresses.py"
+"C:\Users\api_admin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\api_admin\Desktop\FoodicsApiIntegration\ApiIntegration\Branches.py"
+
+echo End Sync: %DATE% %TIME% >> Logs\log_%mmddyyyy%.txt
